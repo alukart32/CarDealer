e46a28501b5744868a62101576659e3490099b1f
alukart32 <alukart32@gmail.com>
2019-02-28 22:26:59 +0300

    Adding simple filter to get specific auto model

--- CarDealer/Controllers/HomeController.cs
+++ CarDealer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using CarDealer.Models.Domain;
+using CarDealer.Models.Purchase;
+using CarDealer.Models.Stock;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,11 +26,14 @@ namespace CarDealer.Controllers
             return View();
         }
 
-        public ActionResult Catalog()
+        public ActionResult Catalog(String str)
         {
             CarContext db = new CarContext();
             //List<Car> products = db.Cars.Where(e => e.manufacturer.Equals("BMW")).ToList();
-            List<Car> cars = db.Cars.ToList();
+            //List<Car> cars = db.Cars.ToList();
+            IQueryable < Car > cars = db.Cars;
+            if (str != "")
+                cars = cars.Where(e => e.manufacturer.Equals(str));
             return View(cars);
         }
 
@@ -41,5 +46,51 @@ namespace CarDealer.Controllers
             return View();
         }
 
+        public ActionResult AddToCart(int? ID = 1)
+        {
+            CarContext db = new CarContext();
+            Car car = db.Cars.Find(ID);
+            // Чтение корзины из сессии
+            ShopBasket myCart = ShopBasket.GetCart(Session["MyCart"]);
+            if (ID != null)
+            {
+               // myCart.AddItem((int)ID, car.ProdName, car.price, 1);
+                // Запись корзины в сессию
+                Session["MyCart"] = myCart;
+            }
+            return Redirect("/Home/Catalog");
+        }
+        
+        public ActionResult CartBrowse()
+        {
+            // Чтение корзины из сессии
+            ShopBasket myCart = ShopBasket.GetCart(Session["MyCart"]);
+            List<ShopBasketPos> lines = (List<ShopBasketPos>)myCart.Lines;
+            return View(lines);
+        }
+        [HttpGet]
+        public ActionResult Buy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Buy(string FirstName, string LastName, string Address)
+        {
+            Stock sdb = new Stock();
+            // Добавляем заказчика
+            Customer c = sdb.AddCustomer(FirstName, LastName, Address);
+            // Читаем корзину из сессии
+            ShopBasket myCart = ShopBasket.GetCart(Session["MyCart"]);
+            // Пытаемся оформить заказ
+            string message;
+            Order o = sdb.CommitTrans(c.customer_id, myCart, out message);
+            // В случае неудачи обнуляем номер
+            int id = (o != null) ? o.order_id : 0;
+            // В случае успеха очищаем сессию с корзиной
+            Session["MyCart"] = (o != null) ? null : Session["MyCart"];
+            // переходим на страницу со статусом
+            return RedirectToAction("OrderStatus", "Home",
+            new { ID = id, msg = message, mail = c.email });
+        }
     }
 }
