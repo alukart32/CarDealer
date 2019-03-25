b6e757fda132a08c5cc2ef8c72e97e33f52156f4
alukart32 <alukart32@gmail.com>
2019-03-25 21:36:22 +0300

    Теперь под Admin можно добавлять авто

--- CarDealer/Models/Users/Controllers/AdminController.cs
+++ CarDealer/Models/Users/Controllers/AdminController.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.AspNet.Identity;
 using CarDealer.Models.Users.Models;
 using System.Threading.Tasks;
+using CarDealer.Models.Domain;
+using CarDealer.Models.Paging;
+using CarDealer.Filter;
 
 namespace CarDealer.Models.Users.Controllers
 {
@@ -148,6 +151,154 @@ namespace CarDealer.Models.Users.Controllers
             {
                 return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
             }
+        }
+
+
+        [Authorize(Roles = "Administrators")]
+        public ActionResult EditEntryCarInDB(int prodID)
+        {
+            CarContext db = new CarContext();
+            Car c = db.Cars.Find(prodID);
+
+            if (c != null)
+            {
+                IQueryable<OrderDetail> orderDetails = db.OrderDetails;
+
+                foreach (OrderDetail o in orderDetails)
+                {
+                    // нашли машину в заказе -> ! не удаляем
+                    if (o.car_id == prodID)
+                    {
+                        return Redirect("AdminCatalog");
+                    }
+                }
+
+                db.Cars.Remove(c);
+                db.SaveChanges();
+            }
+
+            return Redirect("AdminCatalog");
+        }
+
+        [Authorize(Roles = "Administrators")]
+        public ActionResult DeleteFromDB(int prodID)
+        {
+            CarContext db = new CarContext();
+            Car c = db.Cars.Find(prodID);
+
+            if (c != null)
+            {
+                IQueryable<OrderDetail> orderDetails = db.OrderDetails;
+
+                foreach (OrderDetail o in orderDetails)
+                {
+                    if (o.car_id == prodID)
+                    {
+                        return Redirect("AdminCatalog");
+                    }
+                }
+
+                db.Cars.Remove(c);
+                db.SaveChanges();
+            }
+
+            return Redirect("AdminCatalog");
+        }
+        
+        [Authorize(Roles = "Administrators")]
+        public ActionResult AddInDB(String country, String manufacturer, String model, String type, decimal price = 0)
+        {
+            if(country == null & manufacturer == null & model == null & type == null && price == 0)
+            {
+               
+            }
+            else
+            {
+                CarContext db = new CarContext();
+
+                Car c = new Car
+                {
+                    manufacturer = manufacturer,
+                    model = model,
+                    type = type,
+                    price = price,
+                    country = country,
+                    car_id = db.Cars.Count()+1
+                };
+
+                db.Cars.Add(c);
+                db.SaveChanges();
+
+            }
+
+            return View();
+        }
+
+        [Authorize(Roles = "Administrators")]
+        public ActionResult AdminCatalog(String manufacturerList, String relationList, CarFilter carFilter = null, bool restore = false, int page = 1)
+        {
+            CarContext db = new CarContext();
+            IQueryable<Car> cars = db.Cars;
+
+            if (carFilter == null)
+                carFilter = new CarFilter();
+
+            if (restore) // если необходимо восстановить данные, то восстанавливаем из сессии
+                carFilter = CarFilter.GetCarFilter(Session["CarFilter"]);
+
+
+            if (manufacturerList != "" && manufacturerList != null)
+                cars = cars.Where(e => e.manufacturer.Equals(manufacturerList));
+            if (carFilter.model != "" && carFilter.model != null)
+                cars = cars.Where(e => e.model.Equals(carFilter.model));
+            if (carFilter.type != "" && carFilter.type != null)
+                cars = cars.Where(e => e.type.Equals(carFilter.type));
+
+            if (carFilter.price > 0)
+            {
+
+                if (relationList != "")
+                {
+                    decimal d = carFilter.price;
+
+                    switch (relationList)
+                    {
+                        case "<":
+                            cars = cars.Where(e => e.price < d);
+                            break;
+                        case "<=":
+                            cars = cars.Where(e => e.price <= d);
+                            break;
+                        case ">":
+                            cars = cars.Where(e => e.price > d);
+                            break;
+                        case ">=":
+                            cars = cars.Where(e => e.price >= d);
+                            break;
+                        case "=":
+                            cars = cars.Where(e => e.price == d);
+                            break;
+                    }
+                }
+            }
+
+            int pageSize = 5;
+
+            IEnumerable<Car> carsPerPages = cars.ToList().Skip((page - 1) * pageSize).Take(pageSize);
+            CarPageInfo pageInfo = new CarPageInfo { PageNumber = page, PageSize = pageSize, TotalItems = cars.ToList().Count };
+            CarIndexView ivm = new CarIndexView { PageInfo = pageInfo, Cars = carsPerPages };
+
+            // сейвим текущий фильтр в сессию
+            Session["CarFilter"] = carFilter;
+            ivm.carFilter = carFilter;
+
+            return View(ivm);
+        }
+
+        [Authorize(Roles = "Administrators")]
+        public ActionResult AdminIndex()
+        {
+            return View("~/Views/Admin/AdminIndex.cshtml");
         }
     }
 }
